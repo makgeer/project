b706f02445bd7fdb95a6652bda34941233dc2fc5
makgeer <makgeer@gmail.com>
2022-02-07 17:38:45 +0300

    добавление docstring изменение в описании блоков кода.

--- Converter_def.py
+++ Converter_def.py
@@ -1,13 +1,15 @@
-# -*-coding:utf-8-*-
-# пересчет температур
+"""пересчет температур"""
+
+
 def temper(type_temp: int, temp: float) -> float:
     """
     Программа вычисляет температуру приведенную в градусах цельсия
     и возвращает ее в кельвинах или форенгейтах
-    :type.temp1 (int): принимает параметр кельвин
-    :type.temp2 (int): принимает параметр форенгейт
+    :param type_temp: принимает на вход задаваемый пользователем параметр
+    :type type_temp : == 1 принимает параметр кельвин
+    :type type_temp : == 2 принимает параметр форенгейт
     :param temp: тип данных возвращаемый функцией (float)
-    :return: это мне подсказали, почему -1.0 не смог разобраться))
+    :return: возвращает преобразование
 
     """
     if type_temp == 1:
@@ -17,13 +19,16 @@ def temper(type_temp: int, temp: float) -> float:
     return -1.0
 
 
-# пересчет масс
+'# пересчет масс'
+
+
 def massa(type_mass: int, mass: float) -> float:
     """
-
-    :param type_mass:
-    :param mass:
-    :return:
+    Программа принимает на вход вес в метрической системе
+    на вывод преобразует в английскую.
+    :param type_mass: метрическая
+    :param mass: английская
+    :return: возвращает преобразование
     """
     if type_mass == 1:
         return mass * 0.035273998
@@ -42,11 +47,21 @@ def massa(type_mass: int, mass: float) -> float:
     return -1.0
 
 
+'# пересчет длины'
+
+
 def lengths(type_length: int, length: float) -> float:
+    """
+    Программа принимает на вход длину в метрической системе
+    на вывод преобразует в английскую.
+    :param type_length: метрическая система
+    :param length: английская
+    :return: возвращает преобразование
+    """
     if type_length == 1:
-        return length * 1.60934
+        return length / 1.60934
     if type_length == 2:
-        return length * 1.09361
+        return length / 1.09361
     if type_length == 3:
         return length / 0.30481
     if type_length == 4:
@@ -56,26 +71,61 @@ def lengths(type_length: int, length: float) -> float:
     return -1.0
 
 
+def speed_con(type_speed: int, speed: float) -> float:
+    """
+    Программа принимает на вход длину в метрической системе
+    на вывод преобразует в английскую.
+    :param type_speed: метрическая система
+    :param speed: английская
+    :return: возвращает преобразование
+    """
+    if type_speed == 1:
+        return speed / 1.60934  # киломтры в мили
+    if type_speed == 2:
+        return speed * 185.2  # километры в кабельтов
+    if type_speed == 3:
+        return speed / 0.30481  # метры в узлы
+    if type_speed == 4:
+        return speed * 900  # махи в километры
+        return -1.0
+
+
+'# общий модуль логики'
+
+
 def precoint():
     if convert == 1:
         type_temp = int(input("Введите тип температуры:\n 1.Forengate\n 2.Kelvin\n:"))
-        temp = float(input('Введите значение температуы:\n'))
-        return ("Получаем: %.3f" % temper(type_temp, temp))
+        temp = float(input("Введите значение температуы:\n"))
+        return "Получаем: %.3f" % temper(type_temp, temp)
     elif convert == 2:
-        type_mass = int(input("Введите тип массы:  1. Килограммы -> фунты\n 2. Граммы -> унции \n "))
+        type_mass = int(input("Введите тип массы:\n  1. Килограммы -> фунты\n 2. Граммы -> унции \n "))
         mass = float(input("Введите значение массы: "))
-        return ("Получаем: %.3f" % massa(type_mass, mass))
+        return "Получаем: %.3f" % massa(type_mass, mass)
     elif convert == 3:
         type_length = int(input(
-            "Введите тип длинны: ' 1. Километры -> мили\n 2. Метры -> Ярды\n 3. Метры -> футы\n 4. Сантиметры -> дюймы\n 5. Миллиметры -> дюймы \n' "))
+            "Введите тип длинны:\n1. Километры -> мили\n "
+            "2. Метры -> Ярды\n 3. Метры -> футы\n 4. Сантиметры -> дюймы\n"
+            " 5. Миллиметры -> дюймы \n"))
         length = float(input("Введите значение длинны: "))
-        return ("Получаем: %.3f" % lengths(type_length, length))
+        return "Получаем: %.3f" % lengths(type_length, length)
+    elif convert == 4:
+        pass
+        return
+    elif convert == 5:
+        type_speed = int(input(
+            "Ввыберите единицы конвертации:\n1. Колометры в час -> Мили в час\n2. Километры в час -> Кабельтов в час\n"
+            "3. Метр в секунду - Узел в секунду\n 4. Махи в час -> Километры в час"))
+        speed = float(input("Введите значение:\n"))
+        return "Получаем: %.3f" % speed_con(type_speed, speed)
 
 
-# в дальнейшем преобразовать в f-STRING
-# расчет конвертации
-var = int(input('1: Ru - Eng\n2: Eng - Ru\n'))
+'# в дальнейшем преобразовать в f-STRING'
+'# расчет конвертации'
+var = int(input("1: Ru - Eng\n2: Eng - Ru\n"))
 if var == 1:
-    print('Конвертация из метрической в неметрическую')
-    convert = int(input('Что считаем: \n 1. Температура\n 2. Вес\n 3. Длина\n 4. Объем\n 5. Скорость\n '))
+    print("Конвертер 0.2")
+    convert = int(input("Что считаем: \n 1. Температура\n 2. Вес\n "
+                        "3. Длина\n 4. Объем\n 5. Скорость\n "))
+
     print(precoint())
