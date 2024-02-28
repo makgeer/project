85b26c0bbad83ff6af3e1714ca4f3f16c2055efd
makgeer <makgeer@gmail.com>
2024-02-28 19:16:57 +0300

    поправил код в фйлах RU & EN

--- Converter_def.py
+++ Converter_def.py
@@ -1,87 +1,92 @@
 from ru_converter import *
 from en_converter import *
 
-def precoint(var = 1):
-	
-		if convert == 1:
-			type_temp = int(input("Введите тип температуры:\n 1.Forengate\n 2.Kelvin\n:"))
-			temp = float(input("Введите значение температуы:\n"))
-			return "Получаем: %.2f" % (temper(type_temp, temp))
-		elif convert == 2:
-			type_mass = int(input(" Введите тип массы:\n1. Граммы -> унции\n2. Килограммы -> фунты\n"
-				"3. Килограммы -> стоун\n4. Килограмм -> квинтал\n5. Килограмм -> центал\n6. Килограмм -> короткая тонна'us'\n"
-				"7. Килограмм -> длинная тонна 'br'"))
-			mass = float(input("Введите значение массы: "))
-			return "Получаем: %.2f" % (massa(type_mass, mass))
-		elif convert == 3:
-			type_length = int(input(
-				"Введите тип длинны:\n1. Километры -> мили\n"
-				"2. Метры -> Ярды\n3. Метры -> футы\n4. Метры -> дюймы"
-				"5. Сантиметры -> дюймы\n"
-				"6. Миллиметры -> дюймы \n"))
-			length = float(input("Введите значение длинны: "))
-			return "Получаем: %.2f" % (lengths(type_length, length))
-		elif convert == 4:
-			pass
-			return
-		elif convert == 5:
-			type_speed = int(input(
-				"Ввыберите единицы конвертации:\n1. Колометры в час -> Мили в час\n2. Километры в час -> Кабельтов в час\n"
-				"3. Метр в секунду - Узел в секунду\n4. Махи в час -> Километры в час\n"))
-			speed = float(input("Введите значение:\n"))
-			return "Получаем: %.2f" % (speed_con(type_speed, speed))
-	
-def precoint_1(var = 2):
-			
-		if convert == 1:
-			type_temp = int(input("Введите тип температуры:\n 1.Forengate\n 2.Kelvin\n:"))
-			temp = float(input("Введите значение температуы:\n"))
-			return "Получаем: %.2f" % (temper_1(type_temp, temp))
-		elif convert == 2:
-			type_mass = int(input("Введите тип массы:\n1. Граммы <- унции\n2. Килограммы <- фунты\n"
-				"3. Килограммы <- стоун\n4. Килограмм <- квинтал\n5. Килограмм <- центал\n6. Килограмм <- короткая тонна'us'\n"
-				"7. Килограмм <- длинная тонна 'br'\n"))
-			mass = float(input("Введите значение массы: "))
-			return "Получаем: %.2f" % massa_1(type_mass, mass)
-		elif convert == 3:
-			type_length = int(input(
-				"Введите тип длинны:\n1. Километры <- мили\n "
-				"2. Метры <- Ярды\n3. Метры <- футы\n4. Метры <- дюйм"
-				"5. Сантиметры <- дюймы\n"
-				"6. Миллиметры <- дюймы\n"))
-			length = float(input("Введите значение длинны: "))
-			return "Получаем: %.2f" % lengths_1(type_length, length)
-		elif convert == 4:
-			pass
-			return
-		elif convert == 5:
-			type_speed = int(input(
-				"Ввыберите единицы конвертации:\n1. Колометры в час <- Мили в час\n2. Километры в час <- Кабельтов в час\n"
-				"3. Метр в секунду <- Узел в секунду\n4. Махи в час <- Километры в час\n"))
-			speed = float(input("Введите значение:\n"))
-			return "Получаем: %.2f" % speed_con_1(type_speed, speed)
+
+def precount():
+    if convert == 1:
+        type_temp = int(input("Введите тип температуры:\n 1.Forengate\n 2.Kelvin\n:"))
+        temp = float(input("Введите значение температуы:\n"))
+        return "Получаем: %.2f" % (temper(type_temp, temp))
+    elif convert == 2:
+        type_mass = int(input(" Введите тип массы:\n1. Граммы -> унции\n2. Килограммы -> фунты\n"
+                              "3. Килограммы -> стоун\n4. Килограмм -> квинтал\n"
+                              "5. Килограмм -> центал\n6. Килограмм -> короткая тонна'us'\n"
+                              "7. Килограмм -> длинная тонна 'br'"))
+        mass = float(input("Введите значение массы: "))
+        return "Получаем: %.2f" % (massa(type_mass, mass))
+    elif convert == 3:
+        type_length = int(input(
+            "Введите тип длинны:\n1. Километры -> мили\n"
+            "2. Метры -> Ярды\n3. Метры -> футы\n4. Метры -> дюймы"
+            "5. Сантиметры -> дюймы\n"
+            "6. Миллиметры -> дюймы \n"))
+        length = float(input("Введите значение длинны: "))
+        return "Получаем: %.2f" % (lengths(type_length, length))
+    elif convert == 4:
+        type_volume = float(input(
+            "Введите тип объема:\n1. Миллилитр -> американская жидкая унция\n"
+            "2. Миллилитр -> американская житкая чашка\n3. Миллилитр -> американская житкая пинта\n"
+            "4. Литр -> американская житкая кварта\n5. Литр -> американский житкий галлон"
+        ))
+        return
+    elif convert == 5:
+        type_speed = int(input(
+            "Ввыберите единицы конвертации:\n1. Колометры в час -> Мили в час\n2. Километры в час -> Кабельтов в час\n"
+            "3. Метр в секунду - Узел в секунду\n4. Махи в час -> Километры в час\n"))
+        speed = float(input("Введите значение:\n"))
+        return "Получаем: %.2f" % (speed_con(type_speed, speed))
+
+
+def precount_1():
+    if convert == 1:
+        type_temp = int(input("Введите тип температуры:\n 1.Forengate\n 2.Kelvin\n:"))
+        temp = float(input("Введите значение температуы:\n"))
+        return "Получаем: %.2f" % (temper_1(type_temp, temp))
+    elif convert == 2:
+        type_mass = int(input("Введите тип массы:\n1. Граммы <- унции\n2. Килограммы <- фунты\n"
+                              "3. Килограммы <- стоун\n4. Килограмм <- квинтал\n"
+                              "5. Килограмм <- центал\n6. Килограмм "
+                              "<- короткая тонна'us'\n"
+                              "7. Килограмм <- длинная тонна 'br'\n"))
+        mass = float(input("Введите значение массы: "))
+        return "Получаем: %.2f" % massa_1(type_mass, mass)
+    elif convert == 3:
+        type_length = int(input(
+            "Введите тип длинны:\n1. Километры <- мили\n "
+            "2. Метры <- Ярды\n3. Метры <- футы\n4. Метры <- дюйм"
+            "5. Сантиметры <- дюймы\n"
+            "6. Миллиметры <- дюймы\n"))
+        length = float(input("Введите значение длинны: "))
+        return "Получаем: %.2f" % lengths_1(type_length, length)
+    elif convert == 4:
+        pass
+        return
+    elif convert == 5:
+        type_speed = int(input(
+            "Ввыберите единицы конвертации:\n1. Колометры в час <- Мили в час\n2. Километры в час <- Кабельтов в час\n"
+            "3. Метр в секунду <- Узел в секунду\n4. Махи в час <- Километры в час\n"))
+        speed = float(input("Введите значение:\n"))
+        return "Получаем: %.2f" % speed_con_1(type_speed, speed)
 
 
 '# в дальнейшем преобразовать в f-STRING'
 '# расчет конвертации'
+choose = 0
 
-	
-	
 while True:
-	var = input("1: Ru - Eng\n2: Eng - Ru\n Для завершения программы - наберите exit или нажмите клавишу 'q'\n")
-	
-	if var == 'q' or var == 'Q' or var == 'exit' or var == 'Exit':
-		print('Программа завершена, спасибо за использование.\nThe program is complete, thanks for using it.')
-		break
-	var = int(var)
-	if var == 1:
-			print("Конвертер 0.2")
-			convert = int(input("Что считаем: \n 1. Температура\n 2. Вес\n "
-                        "3. Длина\n 4. Объем\n 5. Скорость\n "))
-			print(precoint())
-	elif var == 2:
-		print("Converter 0.2")
-		convert = int(input("What would you like to count:\n1. Temperature\n2. Weight\n3. Lenght\n4."
-						" Volume\n5. Speed\n"))
-		print(precoint_1())
-		
+    choose = input("1: Ru - Eng\n2: Eng - Ru\n Для завершения программы - наберите exit или нажмите клавишу 'q'\n")
+
+    if choose == 'q' or choose == 'Q' or choose == 'exit' or choose == 'Exit':
+        print('Программа завершена, спасибо за использование.\nThe program is complete, thanks for using it.')
+        break
+    choose = int(choose)
+    if choose == 1:
+        print("Конвертер 0.2")
+        convert = int(input("Что считаем: \n 1. Температура\n 2. Вес\n "
+                            "3. Длина\n 4. Объем\n 5. Скорость\n "))
+        print(precount())
+    elif choose == 2:
+        print("Converter 0.2")
+        convert = int(input("What would you like to convertation:\n1. Temperature\n2. Weight\n3. Length\n4."
+                            " Volume\n5. Speed\n"))
+        print(precount_1())
--- en_converter.py
+++ en_converter.py
@@ -1,5 +1,5 @@
 def temper_1(type_temp: int, temp: float) -> float:
-	"""
+    """
     Программа вычисляет температуру приведенную в градусах цельсия
     и возвращает ее в кельвинах или форенгейтах
     :param type_temp: принимает на вход задаваемый пользователем параметр
@@ -9,11 +9,12 @@ def temper_1(type_temp: int, temp: float) -> float:
     :return: возвращает преобразование
 	"""
 
-	if type_temp == 1:
-		return (temp -32) / 1.8
-	elif type_temp == 2:
-		return temp - 273.15
-	return -1.0
+    if type_temp == 1:
+        return (temp - 32) / 1.8
+    elif type_temp == 2:
+        return temp - 273.15
+    return -1.0
+
 
 """ 
 	Пересчет масс
@@ -21,25 +22,26 @@ def temper_1(type_temp: int, temp: float) -> float:
 	значения в метрической системе,
 	возвращает в английской
 """
-	
+
+
 def massa_1(type_mass: int, mass: float) -> float:
-	
-	if type_mass == 1:
-		return mass * 0.035273998 #Граммы -> унции
-	elif type_mass == 2:
-		return mass * 2.2046 #Килограммы -> фунты
-	elif type_mass == 3:
-		return mass * 0.157473 #Килограммы -> стоун
-	elif type_mass == 4:
-		return mass / 50.802 #Килограмм -> квинтал
-	elif type_mass == 5:
-		return mass / 45.36 #Килограмм -> центал
-	elif type_mass == 6:
-		return mass / 907.18 #Килограмм -> короткая тонна
-	elif type_mass == 7:
-		return mass / 1016 #Килограмм -> длинная тонна
-	return -1.0
-	
+    if type_mass == 1:
+        return mass * 0.035273998  # Граммы -> унции
+    elif type_mass == 2:
+        return mass * 2.2046  # Килограммы -> фунты
+    elif type_mass == 3:
+        return mass * 0.157473  # Килограммы -> стоун
+    elif type_mass == 4:
+        return mass / 50.802  # Килограмм -> квинтал
+    elif type_mass == 5:
+        return mass / 45.36  # Килограмм -> центал
+    elif type_mass == 6:
+        return mass / 907.18  # Килограмм -> короткая тонна
+    elif type_mass == 7:
+        return mass / 1016  # Килограмм -> длинная тонна
+    return -1.0
+
+
 """ 
 	Пересчет длин
 	прграмма прринимает на вход 
@@ -47,21 +49,22 @@ def massa_1(type_mass: int, mass: float) -> float:
 	возвращает в английской
 """
 
+
 def lengths_1(type_length: int, length: float) -> float:
-	
-	if type_length == 1:
-		return length * 1.60934 #Километры <- мили
-	if type_length == 2:
-		return length / 1.09361 #Метры <- ярды
-	if type_length == 3: 
-		return length / 3.281 #Метры <- фунты
-	if type_lenght == 4:
-		return leght /39.37 #Метры <- дюймы
-	if type_length == 5:
-		return length * 2.5400013716 #Сантиметры <- дюйм
-	if type_length == 6:
-		return length * 25.4 #Миллиметры <- дюйм
-	return -1.0
+    if type_length == 1:
+        return length * 1.60934  # Километры <- мили
+    if type_length == 2:
+        return length / 1.09361  # Метры <- ярды
+    if type_length == 3:
+        return length / 3.281  # Метры <- фунты
+    if type_length == 4:
+        return length / 39.37  # Метры <- дюймы
+    if type_length == 5:
+        return length * 2.5400013716  # Сантиметры <- дюйм
+    if type_length == 6:
+        return length * 25.4  # Миллиметры <- дюйм
+    return -1.0
+
 
 """ 
 	Пересчет скорости
@@ -70,13 +73,14 @@ def lengths_1(type_length: int, length: float) -> float:
 	возвращает в английской
 """
 
+
 def speed_con_1(type_speed: int, speed: float) -> float:
-if type_speed == 1:
-		return speed * 1.60934  # километры в мили
-	if type_speed == 2:
-		return speed / 5.399  # кмлометры в кабельтов
-	if type_speed == 3:
-		return speed / 0.30481  # километры в узлы
-	if type_speed == 4:
-		return speed * 900  # махи в километры
-	return -1.0
+    if type_speed == 1:
+        return speed * 1.60934  # километры в мили
+    if type_speed == 2:
+        return speed / 5.399  # кмлометры в кабельтов
+    if type_speed == 3:
+        return speed / 0.30481  # километры в узлы
+    if type_speed == 4:
+        return speed * 900  # махи в километры
+    return -1
